f913091fa5e8fe3a06f94f46a53c22d0e339751f
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2022-08-21 19:17:26 +0500

    Update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,43 +1,22 @@
 import React from "react";
 import Card from "./Card.js";
-import e1 from "../images/1.png";
-import e2 from "../images/3.png";
-import e3 from "../images/2.png";
+import data from "../data.js";
 function Experience() {
-  return (
-    <div className="experienceContainer">
+  const cardElements = data.map((item) => {
+    return (
       <Card
-        img={e1}
-        ratings="5.0"
-        count="6"
-        country="USA"
-        title="Life lessons with katie Zaferes"
-        pricing="136"
-        isAvailable={false}
-        isOnline={false}
+        img={item.img}
+        ratings={item.ratings}
+        count={item.count}
+        country={item.country}
+        title={item.title}
+        pricing={item.pricing}
+        isAvailable={item.isAvailable}
+        isOnline={item.isOnline}
       />
-      <Card
-        img={e2}
-        ratings="5.0"
-        count="30"
-        country="USA"
-        title="Learn wedding photography"
-        pricing="125"
-        isAvailable={true}
-        isOnline={true}
-      />
-      <Card
-        img={e3}
-        ratings="4.8"
-        count="3"
-        country="USA"
-        title="Group mountain biking"
-        pricing="50"
-        isAvailable={true}
-        isOnline={false}
-      />
-    </div>
-  );
+    );
+  });
+  return <div className="experienceContainer">{cardElements}</div>;
 }
 
 export default Experience;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+import e1 from "./images/1.png";
+import e2 from "./images/3.png";
+import e3 from "./images/2.png";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    img: e1,
+    ratings: "5.0",
+    count: "6",
+    country: "USA",
+    title: "Life lessons with katie Zaferes",
+    pricing: "136",
+    isAvailable: false,
+    isOnline: false,
+  },
+  {
+    img: e2,
+    ratings: "5.0",
+    count: "30",
+    country: "USA",
+    title: "Learn wedding photography",
+    pricing: "125",
+    isAvailable: true,
+    isOnline: true,
+  },
+  {
+    img: e3,
+    ratings: "4.8",
+    count: "5",
+    country: "USA",
+    title: "Group mountain biking",
+    pricing: "50",
+    isAvailable: true,
+    isOnline: false,
+  },
+];
